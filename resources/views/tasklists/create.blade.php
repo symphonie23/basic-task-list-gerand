f94575b5a8403d184569c9eef4403bf948fd002d
gparawan <102518646+symphonie23@users.noreply.github.com>
2023-03-22 16:18:09 +0800

    added two new buttons for cancelling the creation of tasks and task list

--- resources/views/tasklists/create.blade.php
+++ resources/views/tasklists/create.blade.php
@@ -9,6 +9,9 @@
         <label>Task List Name</label></br>
         <input type="text" name="name" id="name" class="form-control"></br>
         <input type="submit" value="Save" class="btn btn-success"></br>
+        <a href="{{ url('/tasklists') }}" class="btn btn-secondary btn-sm" title="Back to Task Lists">
+        <i class="fa fa-arrow-left" aria-hidden="true"></i> Cancel
+        </a>
     </form>
   
   </div>
