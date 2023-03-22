8a6c3956f49585d57cef2c73c819193818a18368
jbenola <102546460+NEKOtionSite@users.noreply.github.com>
2023-03-22 15:34:42 +0800

    added a new blade files in tasklists file which is the layout.blade.php

---
+++ resources/views/tasklists/layout.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task Application</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+    <!-- CSS -->
+<style>
+body{
+    background: #edf1f5;
+    margin-top:20px;
+}
+.container {
+
+  align-items: center;
+  justify-content: center;
+}
+ 
+ </style>
+ <script>
+</script>
+
+
+
+
+
+
+
+
+
+
+<style>
+
+.col-md-5 {
+    display: inline-block;
+  margin: 0 auto;
+}
+
+.card {
+  text-align: center;
+}
+
+
+</style>
+</head>
+<body>
+<div class="container">
+    @yield('content')
+
+
+</div>
+  
+</body>
+</html>
