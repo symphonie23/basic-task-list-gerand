029efac69d97d76c05f2fb6546c120a0e77bcded
jfrancisco_3 <95389980+NEUTRAL25@users.noreply.github.com>
2023-03-22 17:56:05 +0800

    added an additional files adding Bootstrap Css in the Tasks folder

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -1,28 +1,36 @@
 @extends('tasks.layout')
-@section('content')
-<div class="card">
-  <div class="card-header">Edit Task</div>
-  <div class="card-body">
-      
-      <form action="{{ url('tasks/' .$tasks->id) }}" method="post">
-        {!! csrf_field() !!}
-        @method("PATCH")
-        <input type="hidden" name="id" id="id" value="{{$tasks->id}}" id="id" />
-        <select name="task_list_id" id="task_list_id" class="form-control">
-          @foreach ($task_lists as $task_list)
-            <option value="{{ $task_list->id }}">{{ $task_list->name }}</option>
-          @endforeach
-        </select></br>
-        <label>Task Name:</label></br>
-        <input type="text" name="name" id="name" value="{{$tasks->name}}" class="form-control"></br>
 
-        <input type="submit" value="Update" class="btn btn-success"></br>
-        <!--button to go back to the tasks page-->
-        <a href="{{ url('/tasks') }}" class="btn btn-secondary btn-sm" title="Back to Task Lists">
-            <i class="fa fa-arrow-left" aria-hidden="true"></i> Close
-        </a>
-    </form>
-  
+@section('content')
+
+<div class="container">
+  <div class="col">
+    <div class="col-md-12">
+      <div class="card">
+        <div class="card-header text-center">
+          <h2>Task</h2>
+        </div>
+        <div class="card-body">
+          <form action="{{ url('tasks/' .$tasks->id) }}" method="post">
+            {!! csrf_field() !!}
+            @method("PATCH")
+            <label>Task List:</label></br>
+            <input type="hidden" name="id" id="id" value="{{$tasks->id}}" id="id" />
+            <select name="task_list_id" id="task_list_id" class="form-control">
+              @foreach ($task_lists as $task_list)
+                <option value="{{ $task_list->id }}">{{ $task_list->name }}</option>
+              @endforeach
+            </select></br>
+            <label>Task Name:</label></br>
+            <input type="text" name="name" id="name" value="{{$tasks->name}}" class="form-control"></br>
+            <input type="submit" value="Update" class="btn btn-success">
+            <!--button to go back to the tasklists page-->
+            <a href="{{ url('/tasklists') }}" class="btn btn-secondary btn-md" title="Back to Task Lists">
+              <i class="fa fa-arrow-left" aria-hidden="true"></i> Close
+            </a>
+          </form>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 @stop
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -1,20 +1,22 @@
 @extends('tasks.layout')
 @section('content')
-<div class="card">
-  <div class="card-header">Tasks List</div>
-  <div class="card-body">
-  
-        <div class="card-body">
+<div class="container">
+  <div class="col">
+    <div class="col-md-12">
+      <div class="card">
+        <div class="card-header text-center">
+          <h2>Task</h2>
+        </div>
+      <div class="card-body">
         <h5 class="card-title">Task Name : {{ $tasks->name }}</h5>
-
-  </div>
-
-        <!--button to go back to the tasks page-->
-        <a href="{{ url('/tasks') }}" class="btn btn-secondary btn-sm" title="Back to Task Lists">
-            <i class="fa fa-arrow-left" aria-hidden="true"></i> Back to Tasks
-        </a>
-      
-    </hr>
-  
+      </div>
+      <div class= "m-2">
+      <a href="{{ url('/tasks') }}" class="btn btn-secondary btn-sm" title="Back to Task Lists">
+      <i class="fa fa-arrow-left" aria-hidden="true"></i> Back to Tasks
+      </a>
+      </div>
+      </hr>
+    </div>
   </div>
 </div>
+@endsection
