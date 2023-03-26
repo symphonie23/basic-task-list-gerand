239f16a6dc7c5b88c27a3640fc5503ab9a3c656b
LupusNocti <vinceqcaballero@gmail.com>
2023-03-26 21:16:28 +0800

    fixed the home button color and text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,6 @@
 	<!-- Create a container div to center the button -->
 	<div class="container d-flex justify-content-center align-items-center vh-100" style="margin-top: -150px;">
 		<!-- Add a button using Bootstrap classes -->
-		<a href="{{ url('/tasklists') }}" class="btn btn-primary btn-lg animate-btn">Create Tasks</a>
+		<a href="{{ url('/tasklists') }}" class="btn btn-lg animate-btn" style="background-color: #2AAA8A; color:white;">Create Task Lists</a>
 	</div>
 @endsection
