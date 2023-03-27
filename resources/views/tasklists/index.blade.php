70e351e484d77cfea9f3d4f6590af0856766f664
jbenola <102546460+NEKOtionSite@users.noreply.github.com>
2023-03-27 14:57:16 +0800

    modified changes in resources/views/tasklists/index.blade.php, and show.blade.php

--- resources/views/tasklists/index.blade.php
+++ resources/views/tasklists/index.blade.php
@@ -15,11 +15,13 @@
         </div>
         <div class="card">
                      <div class="table_section">
+                        
                         <table class="table table-striped">
                                 <thead>
                                     <tr class="text-center">
                                         <th class="col-sm-1">ID</th>
-                                        <th class="col-sm-8">Task List Name</th>
+                                        <th class="col-sm-7">Task List Name</th>
+                                        <th class="col-sm-1">Status</th>
                                         <th class="col-sm-3">Actions</th>
                                     </tr>
                                 </thead>
@@ -28,7 +30,18 @@
                                 <tr>
                                 <td class="text-center">{{ $tasklist->id }}</td>
                                     <td>{{ $tasklist->name }}</td>
+                                    <td>
+                                        <h6>8/11</h6>
+                                        <div class="progress progress-xs mt-2">
+                                        <div class="progress-bar bg-success" role="progressbar"
+                                            style="width: 50%"
+                                            aria-valuenow="40"
+                                            aria-valuemin="0"
+                                            aria-valuemax="100">
+                                    </td>
+                                        
                                         <td class="text-center">
+                                            
                                             <a href="{{ url('/tasklists/' . $tasklist->id) }}" title="View Task"><button class="btn btn-outline-info btn-sm m-1"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
                                             <a href="{{ url('/tasklists/' . $tasklist->id . '/edit') }}" title="Edit Task"><button class="btn btn-outline-primary btn-sm m-1"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
                                             <form method="POST" action="{{ url('/tasklists/' . $tasklist->id) }}" accept-charset="UTF-8" style="display:inline">
