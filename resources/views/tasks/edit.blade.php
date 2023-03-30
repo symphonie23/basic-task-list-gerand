468c9281b7bbeb192ba0bbaebebf1d06326a6a4d
vcaballero <102521477+LupusNocti@users.noreply.github.com>
2023-03-30 11:23:39 +0800

    repositioned Task done button in the edit task" q exit

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -25,9 +25,9 @@
             <label>Task Description</label></br>
             <input type="text" name="desc" id="desc" value="{{$tasks->desc}}" class="form-control"></br>
             <label for="deadline">Deadline:</label>
-            <input type="datetime-local" name="deadline_at" id="deadline_at" class="form-control" value="{{ ('deadline_at') }}" />
+            <input type="datetime-local" name="deadline_at" id="deadline_at" class="form-control" value="{{ ('deadline_at') }}" /><br>
             <input class="mx-2 my-2" type="checkbox" name="done" {{ $tasks->finished_at ? 'checked' : '' }}>
-            <label for="done" class= "leads">Task Done</label><br>
+            <label for="done" class= "leads">Task Done</label>
               <!--button to go back to the tasklists page-->
               <input type="submit" value="Update" class="btn float-end" style="background-color: #2AAA8A; color:white;">
               <a href="{{ url('/tasks') }}" class="btn btn-outline-danger float-end mx-2" title="Back to Tasks">Cancel</a>
